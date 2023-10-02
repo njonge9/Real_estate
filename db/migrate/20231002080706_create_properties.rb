9322d31c1dbc59a0bc17bcc1f63cce3c6be0a787
Njonge9 <frednjonge9@gmail.com>
2023-10-02 11:10:46 +0300

    Fix the migrations

--- db/migrate/20231002080706_create_properties.rb
+++ db/migrate/20231002080706_create_properties.rb
@@ -1,6 +1,7 @@
 class CreateProperties < ActiveRecord::Migration[7.0]
   def change
     create_table :properties do |t|
+      t.references :User
       t.string :name
       t.string :address
       t.integer :price
