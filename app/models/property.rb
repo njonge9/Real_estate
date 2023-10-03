e8bfffce198a6d679186b0cdaf2c364692fa2164
Njonge9 <frednjonge9@gmail.com>
2023-10-03 12:16:47 +0300

    Fix the image uploads in the form

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,9 @@
 class Property < ApplicationRecord
   has_one_attached :photo
   belongs_to :user
+
+  def image_as_thumbnail
+    return unless photo.content_type.in?(%w[image/jpeg image/png image/jpg])
+    photo.variant(resize_to_limit: [400, 400]).processed
+  end
 end
