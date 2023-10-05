77b21908aa5b7e9b9309eb675f46cecfd8fdbc8f
Njonge9 <frednjonge9@gmail.com>
2023-10-05 18:08:50 +0300

    Fix the render methods

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,14 +90,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => "onrender.com", protocal: "https"}
+  config.action_mailer.default_url_options = { :host => "zamani.onrender.com", protocal: "https"}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'onrender.com',
+    domain: 'https://zamani.onrender.com',
     user_name: Rails.application.credentials.dig(:google_smtp, :email),
     password: Rails.application.credentials.dig(:google_smtp, :password),
     authentication: 'plain',
