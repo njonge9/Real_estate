368506182c07c8f3e616b2244e7fe7cdf8cbec63
Njonge9 <frednjonge9@gmail.com>
2023-10-05 18:20:35 +0300

    Fix the production host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => "zamani.onrender.com", protocal: "https"}
+  config.action_mailer.default_url_options = { :host => "https://zamani.onrender.com", protocal: "https"}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
