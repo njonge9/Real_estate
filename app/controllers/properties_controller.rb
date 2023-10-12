60208fe4c221071ba9010cdf3e92cdade5e5497f
Njonge9 <frednjonge9@gmail.com>
2023-10-12 12:28:16 +0300

    Add a details column to properties

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -74,6 +74,6 @@ class PropertiesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def property_params
-      params.require(:property).permit(:name, :address, :price, :rooms, :bathrooms, :photo)
+      params.require(:property).permit(:name, :address, :price, :rooms, :bathrooms, :photo, :parking_space)
     end
 end
--- db/migrate/20231012074508_add_parking_space_to_properties.rb
+++ db/migrate/20231012074508_add_parking_space_to_properties.rb
@@ -1,5 +1,5 @@
 class AddParkingSpaceToProperties < ActiveRecord::Migration[7.0]
   def change
-    add_column :properties, :parking_space, :integer
+    add_column :properties, :parking_space, :integer, default: 0
   end
 end
